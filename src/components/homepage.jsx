2dc960f05badfe4aebf9c29574c905cb17813d3b
Tsyriulnikov <max_djvu@mail.ru>
2022-08-16 14:57:49 +0300

    first commit1

---
+++ src/components/homepage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export const Homepage=() =>{
+    <div className="h-1/2 lg:h-screen bg-gradient-to-t from-indigo-200 dark:from-slate-800 dark:to-slate-900 relative overflow-hidden">
+
+    </div>
+}
