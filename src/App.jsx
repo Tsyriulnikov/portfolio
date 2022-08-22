9876570e09632dbf7705408dd346c52ab9d19583
Tsyriulnikov <max_djvu@mail.ru>
2022-08-22 19:53:26 +0300

    services6

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
         <div className='h-screen'>
             <Navbar/>
             <Home/>
-            <About/>
             <Services/>
             <Works/>
             <Contact/>
