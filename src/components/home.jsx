98816f5f65e4776a5f82b094c9cbd6dd94fd031b
Tsyriulnikov <max_djvu@mail.ru>
2022-08-24 18:35:36 +0300

    all 1

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,7 +11,12 @@ export const Home = () => {
     return (
         <div id="home"
             className='h-1/2 lg:h-screen bg-gradient-to-t from-indigo-200 dark:from-slate-800 dark:to-slate-900 relative overflow-hidden'>
-                       <div
+            <img
+                className="absolute bottom-0 right-0 lg:left-0 mx-auto h-4/5 object-cover"
+                src={man}
+                alt=""
+            />
+            <div
                 className="hidden lg:block absolute -bottom-1/4 right-0 left-0 mx-auto w-big h-big bg-indigo-900 rounded-full -z-10"
             > </div>
             <div
