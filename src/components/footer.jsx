c6388730cffb8bacde56c313835d6cf43e735ad3
Tsyriulnikov <max_djvu@mail.ru>
2022-09-07 20:15:00 +0300

    ver24

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,11 +9,7 @@ import linkedin from '../assets/img/linkedin-logo.png'
 export const Footer = () => {
     return (
         <div className="w-full bg-gray-800">
-            <div className="container mx-auto py-5 flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <img className="w-8" src={logo} alt=""/>
-                    <span className="text-2xl font-bold text-white">MaxTs</span>
-                </div>
+
                 <span className="hidden md:block font-medium text-white"
                 >© 2022 Maksim Tsyriulnikov</span
                 >
