d096bf0a470581cfb6cd867204e7ec3952944900
Tsyriulnikov <max_djvu@mail.ru>
2022-09-08 17:35:23 +0300

    ver26

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -7,7 +7,15 @@ export const Works = () => {
         <div id="works" className="py-40 dark:bg-slate-900">
             <div className="container mx-auto">
 
-
+                <div className="flex flex-col gap-3 items-center">
+                    <h1 className="text-indigo-600 font-bold">PORTFOLIO</h1>
+                    <h1 className="text-3xl dark:text-white">Works & Projects</h1>
+                    <p className="w-1/2 text-center text-gray-400">
+                        I help designers, small agencies and businesses bring their ideas to
+                        life. Powered by Figma, VS Code and coffee, I turn your requirements
+                        into a well-designed websites
+                    </p>
+                </div>
 
                 <div className="p-5 sm:p-0 flex flex-wrap justify-between">
 
