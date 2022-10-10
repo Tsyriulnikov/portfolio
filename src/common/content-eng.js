1fe536a42795e6a7bb69f652c5f8766de68b02c8
Tsyriulnikov <max_djvu@mail.ru>
2022-10-10 20:34:57 +0300

    i came back

--- src/common/content-eng.js
+++ src/common/content-eng.js
@@ -1,7 +1,6 @@
-export const homeAboutText=' I am a front-end developer with experience in creating SPA using React, Redux, TypeScript,\n' +
-    '                    JavaScript, SCSS, HTML with future plans to study AngularJS.\n' +
-    '                    I like mobile app development and currently improving my skills\n' +
-    '                    in React Native.\n' +
+export const homeAboutText=' I am a front-end developer with experience in creating SPA using Angular, React, Redux, TypeScript,\n' +
+    '                    JavaScript, SCSS, HTML.\n' +
+    '                    My motto is "It is now or never, do or die!".\n' +
     '                    I spend my spare-time learning English and Spanish.\n' +
     '                    Open for your suggestions!'
 export const homeGreetingsText = 'Greetings, I\'m Maksim Tsyriulnikov'
