8b4854aa4f922ed18fe8cadc744f895a8c8c6d5b
Tsyriulnikov <max_djvu@mail.ru>
2022-11-21 14:40:28 +0300

    final ver1

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -6,8 +6,8 @@ export const Services = () => {
         <div id="services" className="dark:bg-slate-900 py-40">
             <div className="container mx-auto">
                 <div className="flex flex-col gap-3 items-center">
-                    {/*<h1 className="text-indigo-600 font-bold">SERVICES</h1>*/}
-                    {/*<h1 className="text-3xl dark:text-white">What do I offer?</h1>*/}
+                    <h1 className="text-indigo-600 font-bold">SERVICES</h1>
+                    <h1 className="text-3xl dark:text-white">What do I offer?</h1>
                     <p className="w-1/2 text-center text-gray-400">
                         {servicesText}
                     </p>
